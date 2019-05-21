c54ede9afb95547cdb0952de7e93c79857aa74ca
savelgmx <savel@gmx.com>
2019-05-21 15:12:15 +0700

    #commit 4

--- app/src/main/java/fb/fandroid/adv/joindbviewmodelapp/db/DataBase.java
+++ app/src/main/java/fb/fandroid/adv/joindbviewmodelapp/db/DataBase.java
@@ -4,9 +4,10 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
 import fb.fandroid.adv.joindbviewmodelapp.model.Album;
+import fb.fandroid.adv.joindbviewmodelapp.model.Song;
 
 
-@Database(entities = {Album.class}, version = 1)
+@Database(entities = {Album.class,Song.class}, version = 1)
 public abstract class DataBase extends RoomDatabase {
     public abstract MusicDao getMusicDao();
 }
--- app/src/main/java/fb/fandroid/adv/joindbviewmodelapp/db/MusicDao.java
+++ app/src/main/java/fb/fandroid/adv/joindbviewmodelapp/db/MusicDao.java
@@ -9,6 +9,7 @@ import android.arch.persistence.room.Query;
 import java.util.List;
 
 import fb.fandroid.adv.joindbviewmodelapp.model.Album;
+import fb.fandroid.adv.joindbviewmodelapp.model.Song;
 
 /**
  * @author Azret Magometov
@@ -30,5 +31,19 @@ public interface MusicDao {
     //удалить альбом по id
     @Query("DELETE FROM album where id = :albumId")
     void deleteAlbumById(int albumId);
+
+    //***************Song
+    // К таблице Album отношение 1 ко многим
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertSongs(List<Song> songs);
+
+    @Query("DELETE FROM song where id = :songId")
+    int deleteSongById(int songId);
+
+    //*******End Song
+
+
+
+
 
 }
